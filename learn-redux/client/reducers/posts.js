3af4fd5aa4ec6543e6795f56d489e41180a9ce00
Rachel Burke <Rachel.Burke@experian.com>
2019-08-07 13:34:13 -0400

    Add reducers

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,14 @@
+// A reducer takes in two things;
+//      1. the action (information about what happened)
+//      2. a copy of the current state
+
+// action, store
+// okay let me see
+// store
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
