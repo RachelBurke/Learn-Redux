601a02137ce7e3c17482a6fbe2383b74cfcf6cd0
Rachel Burke <Rachel.Burke@experian.com>
2019-08-07 09:01:22 -0400

    Creating Single and PhotoGrid components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,5 +7,7 @@ import css from "./styles/style.styl";
 
 // Import Components
 import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
 
 render(<Main />, document.getElementById("root"));
