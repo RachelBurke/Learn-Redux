da747e1d73e60a6dd2f386ecd1ee2e80cd7afb20
Rachel Burke <Rachel.Burke@experian.com>
2019-08-12 09:43:05 -0400

    Error Tracking and Insights with Sentry

--- learn-redux/client/data/config.js
+++ learn-redux/client/data/config.js
@@ -1,7 +1,7 @@
-import Raven from 'raven-js';
+import Raven from "raven-js";
 
-const sentry_key = 'cb55d4f05cd443ce82303222f77ef5e0';
-const sentry_app = '61499';
+const sentry_key = "1a1c1ddc7cef4981b56697ea5e5413bf";
+const sentry_app = "1528238";
 export const sentry_url = `https://${sentry_key}@app.getsentry.com/${sentry_app}`;
 
 export function logException(ex, context) {
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -12,8 +12,29 @@ import PhotoGrid from "./components/PhotoGrid";
 // Import react router dependencies
 import { Router, Route, IndexRoute } from "react-router";
 import { Provider } from "react-redux";
-
 import store, { history } from "./store";
+
+import Raven from "raven-js";
+import { sentry_url, logException } from "./data/config";
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: "commit123",
+    userLevel: "editior"
+  }
+}).install();
+
+//Using Log Exception
+/*logException(new Error("failure!"), {
+  email: "rburke@butler.edu"
+});
+*/
+
+//test sentry
+//console.log(window.doesNotExist.user);
+
+//Raven.captureMessage("Something bad happened!");
+//Raven.showReportDialog();
 
 const router = (
   <Provider store={store}>
